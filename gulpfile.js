e65c73edf65672f58f8d34ac3da29fdd59c05b5f
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-05-07 11:00:04 +0530

    edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,16 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('node-sass'));
-const del = require('del');
-var cssnano = require('gulp-cssnano');
-var rename = require('gulp-rename');
-
-gulp.task('Compile SASS', () => {
-    return gulp.src('./src/main.scss')
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(gulp.dest('./dist/temp/'));
-});
+import gulp from 'gulp';
+import * as del from 'del';
+import cssnano from 'gulp-cssnano';
+import rename from 'gulp-rename';
 
 gulp.task('Clean', () => {
-    return del([
+    return del.deleteAsync([
         './dist/*',
     ]);
 });
 
-gulp.task('Clean Build', () => {
-    return del([
-        './dist/temp',
-    ]);
-});
-
 gulp.task('Bundle CSS', () => {
-    return gulp.src('./dist/temp/*.css')
+    return gulp.src('./build/*.css')
         .pipe(cssnano())
         .pipe(rename({
             suffix: '.min'
@@ -31,11 +18,10 @@ gulp.task('Bundle CSS', () => {
         .pipe(gulp.dest('./dist/'));
 });
 
-/* gulp.task('Config Release', async() => {
-    return gulp.src([
-            './template/*'
-        ])
-        .pipe(gulp.dest('./dist/'));
-}); */
+gulp.task('Clean Build', () => {
+    return del.deleteAsync([
+        './Build/*',
+    ]);
+});
 
-gulp.task('default', gulp.series(['Clean', 'Compile SASS', 'Bundle CSS', 'Clean Build']));
+gulp.task('default', gulp.series(['Clean', 'Bundle CSS', 'Clean Build']));
